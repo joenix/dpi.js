f8ca5cfa78eb3d4a91c7b1e8dab823a0a5966bc6
Joenix <joenix@qq.com>
2021-10-31 14:51:35 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// dom workflow
+function dom(callback = () => {}) {
+  // create node
+  const node = document.createElement("div");
+
+  // set css
+  node.style.cssText = `width: 1in; height: 1in; position: absolute; left: 0; top: 0; z-index: 2147483647; visibility: hidden;`;
+
+  // append to body
+  document.body.appendChild(node);
+
+  // running callback
+  callback(node);
+
+  // remove node
+  node.parentNode.removeChild(node);
+}
+
+// get screen dpi
+function dpi() {
+  // in deviceXDPI
+  if (window.screen.deviceXDPI) {
+    return {
+      x: window.screen.deviceXDPI,
+      y: window.screen.deviceYDPI
+    };
+  }
+
+  // set cache
+  let cache = {};
+
+  // use dom
+  dom(node => {
+    cache = {
+      x: parseInt(node.offsetWidth),
+      y: parseInt(node.offsetHeight)
+    };
+  });
+
+  // return
+  return cache;
+}
+
+// export
+export default dpi;
---
+++ readme.md
@@ -0,0 +1,24 @@
+# dpi.js
+
+> a package for get screen dpi
+
+## install
+
+```sh
+npm install dpi.js
+# or
+yarn add dpi.js
+```
+
+## use
+
+```js
+// import package
+import dpi from "dpi.js";
+
+// use dpi
+const { x, y } = dpi();
+
+// print
+console.log(x, y);
+```
